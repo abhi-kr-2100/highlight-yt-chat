3ece1cc7d4f43f3292af0bb06dc572953a6770b2
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-02-06 22:10:34 +0530

    Add DEBUG code to hg.js

--- hg.js
+++ hg.js
@@ -1,12 +1,18 @@
+const DEBUG = false;
+
 const authorNameSelector = "#author-id";
 const waitTime = 2000;
 const highlightColor = 'red';
 
 function highlight(names, color) {
     let authors = document.querySelectorAll(authorNameSelector);
+    if (DEBUG) console.log(`${authors.length} people found...`);
+
     for (let author in authors) {
         if (author.textContent in names) {
+            if (DEBUG) console.log(`${author.textContent} found...`);
             author.style.backgroundColor = color;
+            if (DEBUG) console.log(`${author.textContent} highlighted!`);
         }
     }
 }
