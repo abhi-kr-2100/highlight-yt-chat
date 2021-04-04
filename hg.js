7210a0272c22d44c0a2eb99448a04ec3d4e48bfb
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-04-04 12:59:48 +0530

    Remove DEBUG code

--- hg.js
+++ hg.js
@@ -1,21 +1,15 @@
-const DEBUG = false;
-
 const authorNameSelector = "#author-name";
 const waitTime = 2000;
 const highlightColor = 'red';
 
 function highlight(names, color) {
     let authors = document.querySelectorAll(authorNameSelector);
-    if (DEBUG) console.log(`${authors.length} people found...`);
 
     for (let i = 0; i < authors.length; ++i) {
         for (let j = 0; j < names.length; ++j) {
-            if (DEBUG) console.log(`Looking for ${names[j]}...`);
 
             if (authors[i].textContent === names[j]) {
-                if (DEBUG) console.log(`${authors[i].textContent} found...`);
                 authors[i].style.backgroundColor = color;
-                if (DEBUG) console.log(`${authors[i].textContent} highlighted!`);
             }
         }
     }
