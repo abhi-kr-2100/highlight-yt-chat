8fa771b114bf3e20c4a336ef54db0b2987921cf7
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-04-04 13:33:52 +0530

    Reduce the number of lines
    As the user has to copy and paste the souce code each
    time for use, shorter code is better.

--- hg.js
+++ hg.js
@@ -2,28 +2,21 @@ const authorNameSelector = "#author-name";
 const waitTime = 2000;
 const defaultHighlightColor = 'red';
 
-
 keeping_an_eye_on = []
-
 
 function highlight(name, color) {
     let authors = document.querySelectorAll(authorNameSelector);
 
-    for (let i = 0; i < authors.length; ++i) {
-        if (authors[i].textContent === name) {
+    for (let i = 0; i < authors.length; ++i)
+        if (authors[i].textContent === name)
             authors[i].style.backgroundColor = color;
-        }
-    }
 }
-
 
 function keep_an_eye_on(name, color = defaultHighlightColor) {
     keeping_an_eye_on[name] = setInterval(highlight, waitTime, name, color);
 }
 
-
 function clear(name) {
-    if (name in keeping_an_eye_on) {
+    if (name in keeping_an_eye_on)
         clearInterval(keeping_an_eye_on[name]);
-    }
 }
